3e9134c27ababa34fe6488f4ae4e3e3c550b50b4
sean <seanf0617@gmail.com>
2021-08-19 16:27:24 -0400

    Second Attempt

---
+++ tictactoe/player/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView, LogoutView
+
+from .views import home, new_invitation, accept_invitations
+
+urlpatterns = [
+    url('home', home, name='player_home'),
+    url('login', LoginView.as_view(template_name="player/login_form.html"),
+        name="player_login"),
+    url('logout', LogoutView.as_view(),
+        name="player_logout"),
+    url('new_invitation', new_invitation, name="player_new_invitation"),
+    url('accept_invitation/(?P<id>\d+)/$', accept_invitations, name="player_accept_invitation")
+]
---
+++ tictactoe/tictactoe/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+
+def welcome(request):
+    if request.user.is_authenticated:
+        return redirect('player_home')
+    return render(request, 'tictactoe.html')
